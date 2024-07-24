9ed88b50ea3c8a7d96cd42d808959fb9c82123db
Jenish <jpat534@gmail.com>
2024-07-24 12:14:40 +1000

    Createda a function for postiing on the platform

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
+import PostForm from "@/components/postForm";
 import { Button } from "@/components/ui/button";
 import UserInformation from "@/components/userInformation";
 
 export default function Home() {
   return (
-    <div className="grid">
-      <section>
+    <div className="grid grid-cols-8 mt-5 sm:px-5">
+      <section className="hidden md:inline md:colspace-2">
         <UserInformation />
       </section>
 
-      <section>
+      <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
         {/* PostForm */}
+        <PostForm/>
         {/* PostFeed */}
       </section>
 
-      <section>
+      <section className="hidden xl:inline justify-center col-span-2">
         {/* Widget */}
       </section>
     </div>
